416ea2b64bb7a9bded07152d18bb2df9817d4f67
Rohit Sinha <rohitks1@in.ibm.com>
2024-03-06 14:19:41 +0530

    Added navbar

--- angular-project-bootstarp5/src/app/app.module.ts
+++ angular-project-bootstarp5/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BootstrapMessageComponent } from './component/bootstrap-message/bootstrap-message.component';
+import { HeaderToolbarComponent } from './header/header-toolbar/header-toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BootstrapMessageComponent
+    BootstrapMessageComponent,
+    HeaderToolbarComponent
   ],
   imports: [
     BrowserModule,
--- angular-project-bootstarp5/src/app/component/bootstrap-message/bootstrap-message.component.ts
+++ angular-project-bootstarp5/src/app/component/bootstrap-message/bootstrap-message.component.ts
@@ -12,4 +12,7 @@ export class BootstrapMessageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  goToNext(){
+    
+  }
 }
